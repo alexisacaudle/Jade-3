ada54cb8c76f9c7ed5adf64bf30f8a71e9a3e3e2
cauda964 <cauda964@newschool.edu>
2016-04-21 18:30:51 -0400

    Initial Commit

---
+++ Jade 3Tests/Jade_3Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Jade_3Tests.swift
+//  Jade 3Tests
+//
+//  Created by Alexis Caudle on 4/21/16.
+//  Copyright © 2016 Alexis Caudle. All rights reserved.
+//
+
+import XCTest
+@testable import Jade_3
+
+class Jade_3Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
